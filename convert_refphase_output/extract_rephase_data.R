daf5af8e213d8a77adac3e98b74a1dd4e8190a7e
Piotr Pawlik <piotr.pawlik.19@ucl.ac.uk>
2025-03-11 19:27:46 +0000

    remove ascat output

--- convert_refphase_output/extract_rephase_data.R
+++ convert_refphase_output/extract_rephase_data.R
@@ -2,7 +2,6 @@ library(argparse)
 library(fst)
 parser = ArgumentParser(description = "Extract tsv files from refphase output")
 parser$add_argument("--refphase_rData",type = "character",help = "path to refphase .RData file")
-parser$add_argument("--ascat_rds",type = "character",help = "path to ASCAT output")
 parser$add_argument("--output_dir",type = "character",help = "output directory")
 args = parser$parse_args()
 
@@ -13,18 +12,3 @@ purity_ploidy = refphase_results$sample_data
 write.table(phased_segments, file = paste0(args$output_dir, "/phased_segs.tsv"), sep = "\t", row.names = FALSE)
 write.table(snps, file = paste0(args$output_dir, "/phased_snps.tsv"), sep = "\t", row.names = FALSE)
 write.table(purity_ploidy, file = paste0(args$output_dir, "/purity_ploidy.tsv"), sep = "\t", row.names = FALSE)
-
-ascat_output = readRDS(args$ascat_rds)
-df_list <- list()
-for (sample_name in names(ascat_output)) {
-    df <- ascat_output[[sample_name]][['segments_raw']]
-    df_list[[sample_name]] <- df
-}
-ascat_segments <- do.call(rbind, df_list)
-# remove 'chr' prefix from chromosome names:
-ascat_segments$chrom = gsub("chr", "", ascat_segments$chrom)
-# rename column chrom to chr:
-colnames(ascat_segments)[colnames(ascat_segments) == "chrom"] = "chr"
-# add segment column:
-ascat_segments$segment = paste(ascat_segments$chr, ascat_segments$start, ascat_segments$stop, sep = "_")
-write.table(ascat_segments, file = paste0(args$output_dir, "/ascat_segments.tsv"), sep = "\t", row.names = FALSE)
